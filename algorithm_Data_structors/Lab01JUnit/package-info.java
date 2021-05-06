e30b8b56cbf1dbffa25d087462e8009dd4eea79b
KirkHietpas <hietpaskirk@gmail.com>
2021-05-05 22:03:18 -0600

    clean up

--- algorithm_Data_structors/Lab01JUnit/package-info.java
+++
@@ -1,2 +0,0 @@
-package Lab01JUnit;
-
--- algorithm_Data_structors/assignment06/ReallyBadHashFunctor.java
+++
@@ -1,9 +0,0 @@
-package assignment06;
-
-public class ReallyBadHashFunctor implements HashFunctor{
-    @Override
-    public int hash(String item) {
-        char[] chars = item.toCharArray();
-        return (int) chars[0];
-    }
-}
